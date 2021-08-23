20ab408e47e44579c36ea2fe73dbe7f7458b42f7
Angel Rodriguez <54855950+angelRodriguezBetanzos@users.noreply.github.com>
2021-08-23 00:57:59 -0500

    Update UserHasPermissions.js

--- CiudadMujeres/client-app/src/_security/UserHasPermissions.js
+++ CiudadMujeres/client-app/src/_security/UserHasPermissions.js
@@ -1,20 +1,9 @@
-/**
- * This is where all the authorization login is stored
- */
 import Authorization from './Authorization'
 
 export default function UserHasPermissions (router) {
-    /**
-     * Before each route we will see if the current user is authorized
-     * to access the given route
-     */
     router.beforeEach((to, from, next, abort, redirect) => {
         let authorized = false
 
-        /**
-         * Remember that access object in the routes? Yup this why we need it.
-         *
-         */
         if (to.meta !== {}) {
             authorized = Authorization.authorize(
                 to.meta.requiresLogin,
@@ -28,16 +17,10 @@ export default function UserHasPermissions (router) {
             }
 
             if (authorized === 'notAuthorized') {
-                /**
-                 * Redirects to a "default" page
-                 */
                 router.push({ name: 'Error', params: { errorCode: 401 } }).catch((errors) => {
                   })
             }
         }
-        /**
-         * Everything is fine? Let's to the page then.
-         */
         next()
     })
 }
