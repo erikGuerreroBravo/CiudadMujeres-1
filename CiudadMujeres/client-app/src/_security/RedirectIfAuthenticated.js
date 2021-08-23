bd0d39e88204040c62e20cd0c6c0ea9b9e626e7b
Angel Rodriguez <54855950+angelRodriguezBetanzos@users.noreply.github.com>
2021-08-23 00:57:15 -0500

    Update RedirectIfAuthenticated.js

--- CiudadMujeres/client-app/src/_security/RedirectIfAuthenticated.js
+++ CiudadMujeres/client-app/src/_security/RedirectIfAuthenticated.js
@@ -1,15 +1,7 @@
 export default function RedirectIfAuthenticated (router) {
-    /**
-     * If the user is already authenticated he shouldn't be able to visit
-     * pages like login, register, etc...
-     */
     router.beforeEach((to, from, next, abort, redirect) => {
         const user = window.localStorage.getItem('token')
-        /**
-         * Checks if there's a token and the next page name is none of the following
-         */
         if ((user) && (to.name === 'Login' || to.name === 'register')) {
-            // redirects according user role
             router.push({ name: 'Login' })
         }
 
