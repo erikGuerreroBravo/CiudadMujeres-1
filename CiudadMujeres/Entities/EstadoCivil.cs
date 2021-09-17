3ad82ab39d7fd7bfe7b2889422ca1af15f5a18b2
erik guerrero bravo <erik.guerrero.bravo@hotmail.com>
2021-09-17 10:37:10 -0500

    Se agrego el catalogo de estados civiles, con valdiaciones

---
+++ CiudadMujeres/Entities/EstadoCivil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CiudadMujeres.Entities
+{
+    [Table("catEstadosCivil")]
+    public class EstadoCivil
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [StringLength(120, MinimumLength = 3, ErrorMessage = "El campo {0} debe tener entre {1} y {2} caracteres")]
+        public string StrValor { get; set; }
+    }
+}
